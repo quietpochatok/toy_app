b2a3702314f9b4481d47b613e2b4bc1120702e57
quietpochatok <bosroin@gmail.com>
2020-07-08 20:46:12 +0500

    upd: add validates for micropost

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  validates :content, length:{maximum:140}
+  validates :content, length:{maximum:140}, presense: true
+  validates :user_id, presense: true
 end
